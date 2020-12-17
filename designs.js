bb1a1fe576902811a652baef1bd4d0315b85f3a8
Ambar <ambar@calendar42.com>
2020-12-17 13:32:25 +0100

    add option to use table methods to create grid

--- designs.js
+++ designs.js
@@ -5,15 +5,13 @@ var sizeM = 1;
 var sizeN = 1;
 var brushColor = document.querySelector("#colorPicker").value;
 
-function checkRemoveOldGrid(){
-    const existingGrid = document.querySelector('tbody');
+function checkRemoveOldGrid() {
+    const existingGrid = document.querySelector('#pixelCanvas');
     // If the grid does not exist then do nothing, else empty the grid
-    existingGrid === null ? console.log("No old grid to remove") : existingGrid.remove();
+    existingGrid === null ? console.log("No old grid to remove") : existingGrid.innerHTML = "";
 }
 
 const formDataToGrid = function (gridFormData) {
-
-    //TODO: limit width by linking to screen size and log override in console
 
     sizeM = gridFormData.target[0].value;
     sizeN = gridFormData.target[1].value;
@@ -47,7 +45,9 @@ function makeGrid() {
 
     // Your code goes here!
 
-    const newTableBody = document.createElement('tbody');
+    // Select the table element and ready creation of grid
+    const tableArea = document.querySelector('#pixelCanvas');
+    checkRemoveOldGrid();
 
     const respondToTheClick = function (evt) {
         console.log('A cell was clicked. clientX value = ' + evt.clientX + ', clientY value = ' + evt.clientY + ', pageY value = ' + evt.pageY);
@@ -58,8 +58,11 @@ function makeGrid() {
         }
 
     }
-    newTableBody.addEventListener('click', respondToTheClick);
 
+    // OPTION 1 of making the grid
+    /**
+    const newTableBody = document.createElement('tbody');
+    
     for (let i = 0; i < sizeM; i++) {
         const newRow = document.createElement('tr');
         for (let j = 0; j < sizeN; j++) {
@@ -70,10 +73,23 @@ function makeGrid() {
         }
         newTableBody.appendChild(newRow);
     }
-
-    const tableArea = document.querySelector('#pixelCanvas');
-    checkRemoveOldGrid();
-    
     tableArea.appendChild(newTableBody);
+     */
 
+    // OPTION 2 of making the grid
+
+    for (let i = 0; i < sizeM; i++) {
+        let newRow = tableArea.insertRow(-1);
+        for (let j = 0; j < sizeN; j++) {
+            newRow.insertCell(j);
+
+            // The following isnt necessary since the cells do not have any text in them
+            // let newCell = newRow.insertCell(j);
+            // let emptyTextElement = document.createTextNode('');
+            // newCell.appendChild(emptyTextElement);
+        }
+    }
+
+    // Add the event listener to the 
+    tableArea.addEventListener('click', respondToTheClick);
 }
